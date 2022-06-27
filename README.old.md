95e4302ac97a76e96cc720f4ed97a6435e552ced
Gabriel Benjamin <95311293+gbenjaminsc@users.noreply.github.com>
2022-06-26 22:27:50 -0300

    build some structures

---
+++ README.old.md
@@ -0,0 +1,3 @@
+# My site project
+
+Repositório com intuito de armazenar todos os arquivos e informações para meu site onde estará meu portfólio profissional.
---
+++ src/components/Projects/database.jsx
@@ -0,0 +1,38 @@
+
+
+const Projects = [
+    {
+        title: "My Plant",
+        description: "O My Plant tem o conceito de criar uma comunidade para trocar conhecimento e informações sobre as espécies de plantas existentes. O projeto realizado como atividade avaliativa final do curso de Front-End da Kenzie.",
+        link: "https://m3-projeto-capstone-alex-grupo3-k6tt23cft-paulo-david.vercel.app/",
+        github: "https://github.com/gbenjaminsc/M3-ProjetoCapstone--Alex-Grupo3",
+    },
+    {
+        title: "Nu-Kenzie",
+        description: "O conceito era fazer um app para cadastrar despesas e receitas, fazendo um balanço financeiro. Projeto criado como atividade avaliativa para o módulo M3 da Kenzie.",
+        link: "https://react-entrega-s1-nu-kenzie-gbenjaminsc.vercel.app/",
+        github: "https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-s1-nu-kenzie-gbenjaminsc",
+    },
+    {
+        title: "Nu Hamburgueria",
+        description: "Nu Hamburgueria tem o intuito de ser um e-commerce de restaurante com uso de algumas features. Projeto criado como atividade avaliativa para o módulo M3 da Kenzie.",
+        link: "https://react-entrega-s1-hamburgueria-da-kenzie-gbenjaminsc.vercel.app/",
+        github: "https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-s1-hamburgueria-da-kenzie-gbenjaminsc",
+    },
+    {
+        title: "KenzieHub",
+        description: "O Hub é um local onde tem o intuito de cadastrar as tecnologias aprendidas. Projeto criado como atividade avaliativa para o módulo M3 da Kenzie.",
+        link: "https://react-entrega-s2-kenzie-hub-gbenjaminsc.vercel.app/",
+        github: "https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-s2-kenzie-hub-gbenjaminsc",
+    },
+    {
+        title: "KenzieFood",
+        description: "KenzieFood é um app que simula um e-commerce de restaurante. O projeto realizado como atividade avaliativa final do módulo 2 de Front-End da Kenzie. ",
+        link: "https://react-entrega-s2-kenzie-hub-gbenjaminsc.vercel.app/",
+        github: "https://github.com/gbenjaminsc/api-kenziefood-m2-discacciati",
+    }
+
+    
+]
+
+export default Projects
